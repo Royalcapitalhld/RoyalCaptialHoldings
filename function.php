807b5053de17adae6af721ee00816e911ca57f8c
Royalcapitalhld <255387933+Royalcapitalhld@users.noreply.github.com>
2026-03-01 15:36:44 -0500

    Create function.php

---
+++ function.php
@@ -0,0 +1,9 @@
+<?php
+require_once get_template_directory() . '/royalcapitalholdings.inc/setup.php';
+require_once get_template_directory() . '/royalcapitalholdings.inc/enqueue.php';
+require_once get_template_directory() . '/royalcapitalholdings.inc/menus.php';
+require_once get_template_directory() . '/royalcapitalholdings.inc/widgets.php';
+require_once get_template_directory() . '/royalcapitalholdings.inc/customizer.php';
+require_once get_template_directory() . '/royalcapitalholdings.inc/helpers.php';
+require_once get_template_directory() . '/royalcapitalholdings.inc/security.php';
+require_once get_template_directory() . '/royalcapitalholdings.inc/theme-support.php';
